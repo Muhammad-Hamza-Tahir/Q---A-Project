6ae49f54b80b6a5748db2814fc4a86a33935567a
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2021-12-07 17:10:04 +0500

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,15 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2")
+let btn3 = document.getElementById('btn3')
+let para = document.querySelector(".para");
+let para1 = document.getElementById('para1');
+let para2 = document.getElementById('para2') 
+btn1.addEventListener("click",function () {
+    para.classList.toggle('abc')
+})
+btn2.addEventListener('click',function () {
+    para1.classList.toggle('abc')
+})
+btn3.addEventListener('click',function () {
+    para2.classList.toggle('abc')
+})
