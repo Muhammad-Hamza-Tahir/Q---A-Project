2e1d451860e89e5be1f94dfe511cddccb8278efc
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2021-12-10 10:58:10 +0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -6,14 +6,21 @@ let para1 = document.getElementById('para1');
 let para2 = document.getElementById('para2') 
 btn1.addEventListener("click",function () {
     para.classList.toggle('abc')
-    if (m) {
-        
-    }
+    
+    para1.classList.remove('abc');
+    para2.classList.remove('abc');    
+    
    
 })
 btn2.addEventListener('click',function () {
     para1.classList.toggle('abc')
+
+    para.classList.remove('abc');
+    para2.classList.remove('abc');
 })
 btn3.addEventListener('click',function () {
     para2.classList.toggle('abc')
+
+    para.classList.remove('abc');
+    para1.classList.remove('abc');
 })
